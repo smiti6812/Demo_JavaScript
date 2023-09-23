7bb98ed2e89a72fad87fc8222c2b7b790a0e0f4b
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2023-09-23 21:58:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const input_1 = core.getInput('input_1');
-  input_1 = 'I am input_1 put on output';
+  const input = core.getInput('input_1');
+  console.log(input);
+  const output_1 = 'I am output_1';
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  console.log(input_1);
-  console.setOutput('output_1',input_1);
+  console.log(`Hello ${nameToGreet}!`);  
+  console.setOutput('output_1',output_1);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
