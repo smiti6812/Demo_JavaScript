1ef30c448b087450832321c5e8e9a76e1e4a54c9
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2023-09-30 21:12:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
     const { context } = require('@actions/github');
     const randomPos = Math.round(Math.random() * 1000);
     const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-    const response = await fetch(url);
+    const response = await fetch(`${url}&key=${TENOR_TOKEN}`);
     const { results } = await response.json();
     const gifUrl = results[0].media[0].tinygif.url;
     if (!context.payload.action) {
